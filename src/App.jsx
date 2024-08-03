ca20cccc8957350d62f6226aae5d7d68aa90e90c
renzozuk <renzo.zuk19@gmail.com>
2024-08-03 16:39:02 -0300

    Project created and Header component added to the project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './components/Header.jsx';
+/* import "./App.css"; */
+
+function App() {
+    return (
+        <Header />
+    );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+/* import logo from 'https://attic.sh/o0xorl1u1v9cnelqyscs9c8vtyr4'; */
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <img src="https://attic.sh/o0xorl1u1v9cnelqyscs9c8vtyr4" />
+            </div>
+            <div>
+                <h1>Investment Calculator</h1>
+            </div>
+        </header>
+    );
+}
